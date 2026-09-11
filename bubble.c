5e7c067aaceb2b674755b67980aa93946de699ac
LXL741 <2647672095@qq.com>
2026-09-11 18:02:38 +0800

    Add files via upload

---
+++ bubble.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a[]={5, 2, 9, 1, 5, 3};
+    int n=6;
+    int temp;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for (int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
