6150bbbed9695dd9be73fd53817572acf4056ebf
LXL741 <2647672095@qq.com>
2026-09-18 18:16:43 +0800

    Add files via upload
    malloc和free

---
+++ malloc_test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *p = (struct Node
+        *)malloc(sizeof(struct Node));
+        
+        if(p==NULL){
+            printf("内存申请失败！\n");
+            return 0;
+        }
+        p->data=100;
+        p->next=NULL;
+        printf("p->data=%d\n",p->data);
+        free(p);
+        p=NULL;
+        return 0;
+}
